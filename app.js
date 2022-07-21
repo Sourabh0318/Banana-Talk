c2d5bebcbd3c4380d42771770bb77c9163762418
Sourabh Bundele <sourabhbundele@gmail.com>
2022-07-21 21:20:47 +0530

    Updated html, css and js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-// var userName = prompt("Enter your Name:");
-// alert(userName);
 var btn = document.querySelector("#btn");
-var txtInput = document.querySelector("#txt-input"); 
+var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector("#output");
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
-function clickHandler(){
-    console.log("Clicked")
-    console.log(txtInput.value);
+function getTranslation(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error type occured" + error);
+    alert("Server is down try after sometime")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslation(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translate = json.contents.translated;
+            output.innerText = translate
+        })
+        .catch(errorHandler)
 }
 
 btn.addEventListener("click", clickHandler);
--- exercise.js
+++ exercise.js
@@ -1,7 +1,13 @@
 var input = document.querySelector("textarea");
 var primaryBtn = document.querySelector(".btn-primary");
 var inputBtn = document.querySelector("#input-btn");
+var name = document.querySelector("input[name='Translator']")
 
 console.log(input);
 console.log(primaryBtn);
 console.log(inputBtn);
+
+// var userName = prompt("Enter your Name:");
+// alert(userName);
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// output.innerText = "Hello! I am Sourabh " + txtInput.value;
